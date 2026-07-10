863d26c9ce15633ba13db769bb5a6966cebe788c
Jewwey <wangjerry1007@gmail.com>
2026-07-10 17:12:30 -0600

    Fetch derms and term id

--- src/ubc-data/scripts/fetch-subjects.js
+++ src/ubc-data/scripts/fetch-subjects.js
@@ -57,7 +57,7 @@ async function main() {
 			if (!code) return;
 
 			subjects[code] = id;
-			console.log(`  ${id} -> ${code}`);
+			console.log(`${id} -> ${code}`);
 		});
 
 		await sleep(DELAY);
---
+++ src/ubc-data/scripts/fetch-terms.js
@@ -0,0 +1,35 @@
+// node scripts/fetch-terms.js
+
+import { writeFile } from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+
+const API_URL = "https://courses.students.ubc.ca/jsonapi/taxonomy_term/term";
+
+async function main() {
+    const terms = {};
+
+    const res = await fetch(API_URL);
+    if (!res.ok) {
+        console.warn(`HTTP ${res.status}, skipping`);
+        return null;
+    }
+
+    const json = await res.json();
+    if (!json.data || json.data.length === 0) return null;
+
+    // fetch each term name and id in data
+    json.data.forEach(term => {
+        const name = term.attributes.name;
+        const id = term.attributes.drupal_internal__tid;
+
+        terms[name] = id;
+        console.log(`${id} -> ${name}`)
+    });
+
+    const outPath = fileURLToPath(new URL("../data/terms.json", import.meta.url));
+    await writeFile(outPath, JSON.stringify(terms, null, 2));
+
+    console.log(`\nSaved ${Object.keys(terms).length} terms to data/terms.json`);
+}
+
+main();
